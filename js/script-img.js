28dd9902061cbb62d8d72d01ed12f9975c3ace4b
Дмитрий Дикк <dmitrydev91@gmail.com>
2021-07-30 19:38:19 +0400

    add scroll-top item

---
+++ js/script-img.js
@@ -0,0 +1,116 @@
+document.addEventListener('DOMContentLoaded', () => {
+    /* start -------------------------------- Скрипты для секции .calculator-section -------------------------------- */
+    const windowsTypes = document.querySelectorAll('.calculator__list li');
+    const windowImg = document.querySelector('.calculator__img');
+
+    if (windowsTypes && windowImg) {
+        windowsTypes.forEach(window => {
+            window.addEventListener('click', function() {
+                const dataWindow = window.dataset.window;
+                const dataWindowImg = window.dataset.windowImg;
+                windowsTypes.forEach(window => {
+                    window.classList.remove('active');
+                });
+                this.classList.add('active');
+                windowImg.src = dataWindow;
+                windowImg.removeAttribute('class');
+                windowImg.classList.add('calculator__img');
+                windowImg.classList.add(dataWindowImg);
+            });
+        });
+    }  
+    /* end -------------------------------- Скрипты для секции .calculator-section ---------------------------------- */
+
+    /* start ---------------------------------- Скрипты для секции .choose-own-color -------------------------------- */
+    const colorsFrame = document.querySelectorAll('[data-frame="frame"]'),
+        colorsAccessory = document.querySelectorAll('[data-accessory="accessory"]'),
+        chooseOwnColorContainer = document.querySelector('.choose-own-color__container'),
+        chooseOwnColorAccessoryBox = document.querySelector('.choose-own-color__accessory-box');
+
+    /* Скрипт для блока choose-own-color добавялет или убирает класс active у кликнутого элемента, заменяет изображения двери и ручки */
+    function activeChooseOwnColor(colors) {
+        colors.forEach((color) => {
+            color.addEventListener('click', function() {
+                if (color.dataset.color === 'silve') {
+                    chooseOwnColorAccessoryBox.style.backgroundImage = 'url("img/choose-own-bg7.png")';
+                } else if (color.dataset.color === 'gold') {
+                    chooseOwnColorAccessoryBox.style.backgroundImage = 'url("img/choose-own-bg8.png")';
+                } else if (color.dataset.color === 'bronze') {
+                    chooseOwnColorAccessoryBox.style.backgroundImage = 'url("img/choose-own-bg9.png")';
+                } else if (color.dataset.color === 'titan') {
+                    chooseOwnColorAccessoryBox.style.backgroundImage = 'url("img/choose-own-bg10.png")';
+                } else if (color.dataset.color === 'white-gold') {
+                    chooseOwnColorAccessoryBox.style.backgroundImage = 'url("img/choose-own-bg11.png")';
+                } else if (color.dataset.color === 'copper') {
+                    chooseOwnColorAccessoryBox.style.backgroundImage = 'url("img/choose-own-bg12.png")';
+                }
+                if (window.matchMedia("(max-width: 767px)").matches) {
+                    if (color.dataset.color === 'cherry') {
+                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-1.png') no-repeat top 206px right -336px/520px";
+                    } else if (color.dataset.color === 'oak') {
+                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-2.png') no-repeat top 206px right -336px/520px";
+                    } else if (color.dataset.color === 'chestnut') {
+                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-3.png') no-repeat top 206px right -336px/520px";
+                    } else if (color.dataset.color === 'pine') {
+                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-4.png') no-repeat top 206px right -336px/520px";
+                    } else if (color.dataset.color === 'alder') {
+                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-5.png') no-repeat top 206px right -336px/520px";
+                    } else if (color.dataset.color === 'maple') {
+                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-6.png') no-repeat top 206px right -336px/520px";
+                    }
+                }
+                if (window.matchMedia("(min-width: 768px) and (max-width: 1023px)").matches) {
+                    if (color.dataset.color === 'cherry') {
+                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-1.png') no-repeat top 0 right -105px/329px";
+                    } else if (color.dataset.color === 'oak') {
+                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-2.png') no-repeat top 0 right -105px/343px";
+                    } else if (color.dataset.color === 'chestnut') {
+                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-3.png') no-repeat top 0 right -105px/356px";
+                    } else if (color.dataset.color === 'pine') {
+                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-4.png') no-repeat top 0 right -105px/356px";
+                    } else if (color.dataset.color === 'alder') {
+                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-5.png') no-repeat top 0 right -105px/356px";
+                    } else if (color.dataset.color === 'maple') {
+                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-6.png') no-repeat top 0 right -105px/356px";
+                    }
+                }
+                if (window.matchMedia("(min-width: 1024px)").matches) {
+                    if (color.dataset.color === 'cherry') {
+                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-1.png') no-repeat top 0 right 0/322px";
+                    } else if (color.dataset.color === 'oak') {
+                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-2.png') no-repeat top 0 right 0/331px";
+                    }  else if (color.dataset.color === 'chestnut') {
+                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-3.png') no-repeat top 0 right 0/343px";
+                    } else if (color.dataset.color === 'pine') {
+                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-4.png') no-repeat top 0 right 0/343px";
+                    } else if (color.dataset.color === 'alder') {
+                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-5.png') no-repeat top 0 right 0/343px";
+                    } else if (color.dataset.color === 'maple') {
+                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-6.png') no-repeat top 0 right 0/343px";
+                    }                         
+                }                
+                colors.forEach((color) => {
+                    color.classList.remove('active');
+                });
+                this.classList.add('active');
+            });
+        });
+    };
+
+    if (chooseOwnColorContainer) {
+        chooseOwnColorContainer.classList.remove('active');
+        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-1.png') no-repeat top 206px right -336px/520px";
+        
+        if (window.matchMedia("(min-width: 768px)").matches) {
+            chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-1.png') no-repeat top 0 right -105px/334px";                    
+        }
+
+        if (window.matchMedia("(min-width: 1024px)").matches) {
+            chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-1.png') no-repeat top 0 right 0/322px";  
+        }
+
+        activeChooseOwnColor(colorsFrame);
+        activeChooseOwnColor(colorsAccessory);
+    }
+    /* end ---------------------------------- Скрипты для секции .choose-own-color ---------------------------------- */
+});
--- js/script.js
+++ js/script.js
@@ -29,13 +29,26 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     /* end -------------------------------- Скрипты для секции .header ---------------------------------------------- */
 
+    /* start -------------------------------- Скрипты для кнопки вверх .scroll-top ---------------------------------- */
+    const ScrollTopBtn = document.querySelector('.scroll-top');
+
+    if (ScrollTopBtn) {
+        ScrollTopBtn.addEventListener('click', () => {
+            window.scrollTo(0, 0);
+        });
+
+        window.addEventListener('scroll', () => {
+            const topOffset = window.pageYOffset;
+            (topOffset >= 500) ? ScrollTopBtn.classList.add('active') : ScrollTopBtn.classList.remove('active');
+        });
+    }
+    /* end -------------------------------- Скрипты для кнопки вверх .scroll-top ------------------------------------ */
+
     /* start -------------------------------- Скрипты для секции .calculator-section -------------------------------- */
     const rangeHeight = document.querySelector('.calculator__range-height');
     const rangeWidth = document.querySelector('.calculator__range-width');
     const inputHeight = document.querySelector('.calculator__input.input-height');
     const inputWidth = document.querySelector('.calculator__input.input-width');
-    const windowsTypes = document.querySelectorAll('.calculator__list li');
-    const windowImg = document.querySelector('.calculator__img');
 
     function initRange(range, input) {
         noUiSlider.create(range, {
@@ -67,24 +80,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (rangeHeight, inputHeight, rangeWidth, inputWidth) {
         initRange(rangeHeight, inputHeight);
-        initRange(rangeWidth, inputWidth);
-
-        if (windowsTypes && windowImg) {
-            windowsTypes.forEach(window => {
-                window.addEventListener('click', function() {
-                    const dataWindow = window.dataset.window;
-                    const dataWindowImg = window.dataset.windowImg;
-                    windowsTypes.forEach(window => {
-                        window.classList.remove('active');
-                    });
-                    this.classList.add('active');
-                    windowImg.src = dataWindow;
-                    windowImg.removeAttribute('class');
-                    windowImg.classList.add('calculator__img');
-                    windowImg.classList.add(dataWindowImg);
-                });
-            });
-        }        
+        initRange(rangeWidth, inputWidth);      
     }
     /* end -------------------------------- Скрипты для секции .calculator-section ---------------------------------- */
 
@@ -205,99 +201,6 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
     /* end ---------------------------------- Скрипты для секции .profile-section ----------------------------------- */
-
-    /* start ---------------------------------- Скрипты для секции .choose-own-color -------------------------------- */
-    const colorsFrame = document.querySelectorAll('[data-frame="frame"]'),
-        colorsAccessory = document.querySelectorAll('[data-accessory="accessory"]'),
-        chooseOwnColorContainer = document.querySelector('.choose-own-color__container'),
-        chooseOwnColorAccessoryBox = document.querySelector('.choose-own-color__accessory-box');
-
-    /* Скрипт для блока choose-own-color добавялет или убирает класс active у кликнутого элемента, заменяет изображения двери и ручки */
-    function activeChooseOwnColor(colors) {
-        colors.forEach((color) => {
-            color.addEventListener('click', function() {
-                if (color.dataset.color === 'silve') {
-                    chooseOwnColorAccessoryBox.style.backgroundImage = 'url("img/choose-own-bg7.png")';
-                } else if (color.dataset.color === 'gold') {
-                    chooseOwnColorAccessoryBox.style.backgroundImage = 'url("img/choose-own-bg8.png")';
-                } else if (color.dataset.color === 'bronze') {
-                    chooseOwnColorAccessoryBox.style.backgroundImage = 'url("img/choose-own-bg9.png")';
-                } else if (color.dataset.color === 'titan') {
-                    chooseOwnColorAccessoryBox.style.backgroundImage = 'url("img/choose-own-bg10.png")';
-                } else if (color.dataset.color === 'white-gold') {
-                    chooseOwnColorAccessoryBox.style.backgroundImage = 'url("img/choose-own-bg11.png")';
-                } else if (color.dataset.color === 'copper') {
-                    chooseOwnColorAccessoryBox.style.backgroundImage = 'url("img/choose-own-bg12.png")';
-                }
-                if (window.matchMedia("(max-width: 767px)").matches) {
-                    if (color.dataset.color === 'cherry') {
-                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-1.png') no-repeat top 206px right -336px/520px";
-                    } else if (color.dataset.color === 'oak') {
-                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-2.png') no-repeat top 206px right -336px/520px";
-                    } else if (color.dataset.color === 'chestnut') {
-                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-3.png') no-repeat top 206px right -336px/520px";
-                    } else if (color.dataset.color === 'pine') {
-                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-4.png') no-repeat top 206px right -336px/520px";
-                    } else if (color.dataset.color === 'alder') {
-                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-5.png') no-repeat top 206px right -336px/520px";
-                    } else if (color.dataset.color === 'maple') {
-                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-6.png') no-repeat top 206px right -336px/520px";
-                    }
-                }
-                if (window.matchMedia("(min-width: 768px) and (max-width: 1023px)").matches) {
-                    if (color.dataset.color === 'cherry') {
-                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-1.png') no-repeat top 0 right -105px/329px";
-                    } else if (color.dataset.color === 'oak') {
-                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-2.png') no-repeat top 0 right -105px/343px";
-                    } else if (color.dataset.color === 'chestnut') {
-                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-3.png') no-repeat top 0 right -105px/356px";
-                    } else if (color.dataset.color === 'pine') {
-                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-4.png') no-repeat top 0 right -105px/356px";
-                    } else if (color.dataset.color === 'alder') {
-                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-5.png') no-repeat top 0 right -105px/356px";
-                    } else if (color.dataset.color === 'maple') {
-                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-6.png') no-repeat top 0 right -105px/356px";
-                    }
-                }
-                if (window.matchMedia("(min-width: 1024px)").matches) {
-                    if (color.dataset.color === 'cherry') {
-                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-1.png') no-repeat top 0 right 0/322px";
-                    } else if (color.dataset.color === 'oak') {
-                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-2.png') no-repeat top 0 right 0/331px";
-                    }  else if (color.dataset.color === 'chestnut') {
-                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-3.png') no-repeat top 0 right 0/343px";
-                    } else if (color.dataset.color === 'pine') {
-                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-4.png') no-repeat top 0 right 0/343px";
-                    } else if (color.dataset.color === 'alder') {
-                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-5.png') no-repeat top 0 right 0/343px";
-                    } else if (color.dataset.color === 'maple') {
-                        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-6.png') no-repeat top 0 right 0/343px";
-                    }                         
-                }                
-                colors.forEach((color) => {
-                    color.classList.remove('active');
-                });
-                this.classList.add('active');
-            });
-        });
-    };
-
-    if (chooseOwnColorContainer) {
-        chooseOwnColorContainer.classList.remove('active');
-        chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-1.png') no-repeat top 206px right -336px/520px";
-        
-        if (window.matchMedia("(min-width: 768px)").matches) {
-            chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-1.png') no-repeat top 0 right -105px/334px";                    
-        }
-
-        if (window.matchMedia("(min-width: 1024px)").matches) {
-            chooseOwnColorContainer.style.background = "#F9F9F9 url('img/choose-own-bg-1.png') no-repeat top 0 right 0/322px";  
-        }
-
-        activeChooseOwnColor(colorsFrame);
-        activeChooseOwnColor(colorsAccessory);
-    }
-    /* end ---------------------------------- Скрипты для секции .choose-own-color ---------------------------------- */
 
     /* start ---------------------------------- Скрипты для секции .sketches-section---------------------------------- */
     const sketchesInput = document.querySelector('.form-sketches--1 .form-sketches__none');
